456ab476aa362cfd89620af9ff5b30b7d985ac56
Michal Bukovský <michal.bukovsky@seznam.cz>
2021-12-09 16:32:59 +0100

    day 9 part 2

--- app/days/09/SmokeBasin.php
+++ app/days/09/SmokeBasin.php
@@ -26,7 +26,21 @@ class SmokeBasin extends TwoPartRunner
 
     protected function runPart2(Input $data): string
     {
-        return '';
+        $basins = [];
+
+        $map = $data->getAsArrayOfArrayOfIntegers(true);
+        foreach ($map as $y => $line) {
+            foreach ($line as $x => $value) {
+                $basinSize = $this->findBasin($map, $x, $y) - 1;    // no idea why -1, but it works.
+                if ($basinSize !== 0) {
+                    $basins[] = $basinSize;
+                }
+            }
+        }
+
+        rsort($basins);
+
+        return (string)($basins[0] * $basins[1] * $basins[2]);
     }
 
 
@@ -53,6 +67,31 @@ class SmokeBasin extends TwoPartRunner
     }
 
 
+    private function findBasin(array &$map, int $xCenter, int $yCenter): int
+    {
+        $basinSize = 0;
+        $map[$yCenter][$xCenter] = null;
+
+        if ($map[$yCenter][$xCenter] === 9) {
+            return $basinSize;
+        }
+
+        $basinSize++;
+
+        for ($rad = 0; $rad < 2 * M_PI; $rad += M_PI / 2) {
+            $x = $xCenter + (int)sin($rad);
+            $y = $yCenter + (int)cos($rad);
+            if (!isset($map[$y][$x]) || $map[$y][$x] === 9) {
+                continue;
+            }
+
+            $basinSize += $this->findBasin($map, $x, $y);
+        }
+
+        return $basinSize;
+    }
+
+
     protected function getExpectedTestResult1(): ?string
     {
         return '15';
@@ -61,6 +100,6 @@ class SmokeBasin extends TwoPartRunner
 
     protected function getExpectedTestResult2(): ?string
     {
-        return '';
+        return '1134';
     }
 }
